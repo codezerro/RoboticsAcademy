df610272e0a9589576479bd387ec06a938610b76
codezerro <kabircp08@gmail.com>
2024-10-13 12:06:23 +0600

    update restapi and auto completion

--- academy/academy_rest_api/urls.py
+++ academy/academy_rest_api/urls.py
@@ -1,8 +1,12 @@
 from rest_framework import routers
+from django.urls import path
 
 from academy.academy_rest_api.views.exercises import ExerciseViewSet
+from academy.academy_rest_api.views.exercises import ExerciseViewSet2
 
 router = routers.SimpleRouter()
 router.register(r'exercises', ExerciseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('test/', ExerciseViewSet2.as_view(), name='hello-view'),
+]
--- academy/academy_rest_api/views/exercises.py
+++ academy/academy_rest_api/views/exercises.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 from academy.academy_rest_api.serializers.exercises import ExerciseSerializer
 from exercises.models import Exercise
@@ -10,3 +13,21 @@ class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+
+class ExerciseViewSet2(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # No authentication required
+
+    def get(self, request):
+        # This will return a JSON response with `{"name": "hello"}`
+        return Response({"name": "hello"})
+    def post(self, request):
+        # Extract data from the request (e.g., `name` from the body)
+        name = request.data.get("name", None)
+        
+        if name:
+            # Return a response with the received data
+            return Response({"message": f"Hello, {name}!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "Name not provided"}, status=status.HTTP_400_BAD_REQUEST)
